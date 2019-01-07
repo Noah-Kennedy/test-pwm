9a645fa3aca1658cd2f101bd3dea54aaab660402
noah <nomaxx117@gmail.com>
2019-01-07 17:11:00 -0600

    Fixed everything

--- enable-pwm.sh
+++ enable-pwm.sh
@@ -2,6 +2,8 @@
 sudo bash -c "echo am33xx_pwm > /sys/devices/platform/bone_capemgr/slots"
 sudo bash -c "echo cape-universal > /sys/devices/platform/bone_capemgr/slots"
 
+config-pin P8_13 pwm
+
 #sudo bash -c "echo 1 > /sys/class/pwm/pwmchip0/export"
 #sudo bash -c "echo 1 > /sys/class/pwm/pwmchip1/export"
 #sudo bash -c "echo 1 > /sys/class/pwm/pwmchip2/export"
