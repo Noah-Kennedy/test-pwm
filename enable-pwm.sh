bb8dc2627fdfea56b1f94aa6f050d656cca3795c
noah <nomaxx117@gmail.com>
2019-01-07 15:39:53 -0600

    code

--- enable-pwm.sh
+++ enable-pwm.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-echo am33xx_pwm > /sys/devices/platform/bone_capemgr/slots
-echo cape-universal > /sys/devices/platform/bone_capemgr/slots
-echo 1 > /sys/class/pwm/pwmchip6/export
+sudo bash -c "echo am33xx_pwm > /sys/devices/platform/bone_capemgr/slots"
+sudo bash -c "echo cape-universal > /sys/devices/platform/bone_capemgr/slots"
+
+#sudo bash -c "echo 1 > /sys/class/pwm/pwmchip0/export"
+#sudo bash -c "echo 1 > /sys/class/pwm/pwmchip1/export"
+#sudo bash -c "echo 1 > /sys/class/pwm/pwmchip2/export"
+#sudo bash -c "echo 1 > /sys/class/pwm/pwmchip3/export"
+#sudo bash -c "echo 1 > /sys/class/pwm/pwmchip4/export"
+#sudo bash -c "echo 1 > /sys/class/pwm/pwmchip5/export"
+sudo bash -c "echo 1 > /sys/class/pwm/pwmchip6/export"
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate sysfs_pwm;
-use sysfs_pwm::{Pwm, Result};
-use std::time::Duration;
-use std::thread::sleep;
+
 use std::process::Command;
+use std::thread::sleep;
+use std::time::Duration;
+
+use sysfs_pwm::{Pwm, Result};
 
 // PIN: EHRPWM2B (P8_13)
 const BB_PWM_CHIP: u32 = 6;
@@ -12,11 +14,9 @@ const DUR: u64 = 1;
 /// Make an LED "breathe" by increasing and
 /// decreasing the brightness
 fn main() {
-    enable_pwm();
-
     let pwm = Pwm::new(BB_PWM_CHIP, BB_PWM_NUMBER).unwrap(); // number depends on chip, etc.
     loop {
-	println!("High");
+        println!("High");
         pwm.with_exported(|| {
             pwm.enable(true).unwrap();
             pwm.set_period_ns(20_000).unwrap();
@@ -42,5 +42,4 @@ pub fn enable_pwm() {
         arg("enable-pwm.sh")
         .output()
         .expect("Failed to enable pwm driver!");
-
 }
