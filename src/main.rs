3b67df8efb49003938ea2a8e38ff8a55445e39d2
noah <nomaxx117@gmail.com>
2019-01-07 18:34:48 -0600

    Added nonworking breathe

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ const DUR: u64 = 1;
 /// Make an LED "breathe" by increasing and
 /// decreasing the brightness
 fn main() {
+    blink();
+}
+
+fn blink() {
     let pwm = Pwm::new(BB_PWM_CHIP, BB_PWM_NUMBER).unwrap(); // number depends on chip, etc.
     loop {
         println!("High");
@@ -36,6 +40,44 @@ fn main() {
 
         sleep(Duration::new(DUR, 0));
     }
+}
+
+fn pwm_increase_to_max(pwm: &Pwm,
+                       duration_ms: u32,
+                       update_period_ms: u32) -> Result<()> {
+    let step: f32 = duration_ms as f32 / update_period_ms as f32;
+    let mut duty_cycle = 0.0;
+    let period_ns: u32 = pwm.get_period_ns()?;
+    while duty_cycle < 1.0 {
+        pwm.set_duty_cycle_ns((duty_cycle * period_ns as f32) as u32)?;
+        duty_cycle += step;
+    }
+    pwm.set_duty_cycle_ns(period_ns)
+}
+
+fn pwm_decrease_to_minimum(pwm: &Pwm,
+                           duration_ms: u32,
+                           update_period_ms: u32) -> Result<()> {
+    let step: f32 = duration_ms as f32 / update_period_ms as f32;
+    let mut duty_cycle = 1.0;
+    let period_ns: u32 = pwm.get_period_ns()?;
+    while duty_cycle > 0.0 {
+        pwm.set_duty_cycle_ns((duty_cycle * period_ns as f32) as u32)?;
+        duty_cycle -= step;
+    }
+    pwm.set_duty_cycle_ns(0)
+}
+
+fn breathe() {
+    let pwm = Pwm::new(BB_PWM_CHIP, BB_PWM_NUMBER).unwrap(); // number depends on chip, etc.
+    pwm.with_exported(|| {
+        pwm.enable(true).unwrap();
+        pwm.set_period_ns(20_000).unwrap();
+        loop {
+            pwm_increase_to_max(&pwm, 10000, 20).unwrap();
+            pwm_decrease_to_minimum(&pwm, 10000, 20).unwrap();
+        }
+    }).unwrap();
 }
 
 pub fn enable_pwm() {
